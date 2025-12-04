9df4d970c1678243b4a66d1dd0378f184cf5500d
anand maharana <anandmaharana427@gmail.com>
2025-12-04 19:28:07 +0530

    feat: set up Express server with health check endpoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("setup file and folders");
+import express from "express";
+import process from "process";
+import { asyncHandler } from "./utils/AsyncHandler.js";
+import { ApiResponse } from "./utils/ApiResponse.js";
+import { globalErrorHandler } from "./middleware/globalErrorHandler.js";
+
+const app = express();
+
+app.get(
+  "/health",
+  asyncHandler(async (req, res) => {
+    res.json(new ApiResponse(200, null, "Server health is good"));
+  }),
+);
+
+app.use(globalErrorHandler);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Application listen on port : ${PORT}`);
+});
