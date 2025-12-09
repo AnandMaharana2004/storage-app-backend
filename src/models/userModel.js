bbb66a1517a7344d388617d0367ccbaefe6e7eb7
anand maharana <anandmaharana427@gmail.com>
2025-12-09 17:37:31 +0530

    feat: implement password hassing befor save with bcrypt

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -53,6 +53,16 @@ const userSchema = new Schema(
   },
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
 const User = model("User", userSchema);
 
 export default User;
