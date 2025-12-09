037220bf3eecf784785ab14de3357cdd13d2f41b
anand maharana <anandmaharana427@gmail.com>
2025-12-09 17:34:16 +0530

    feat: create necessary database models

---
+++ src/models/userModel.js
@@ -0,0 +1,58 @@
+import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: [
+        3,
+        "name field should a string with at least three characters",
+      ],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,
+        "please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      minLength: 4,
+    },
+    rootDirId: {
+      type: Schema.Types.ObjectId,
+      ref: "Directory",
+    },
+    picture: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/002/318/271/non_2x/user-profile-icon-free-vector.jpg",
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Manager", "User"],
+      default: "User",
+    },
+    maxStorageInBytes: {
+      type: Number,
+      required: true,
+      default: 15 * 1024 ** 3,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    strict: "throw",
+  },
+);
+
+const User = model("User", userSchema);
+
+export default User;
