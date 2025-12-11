47d7bb9495c3e747c603880076469d535fd7818d
anand maharana <anandmaharana427@gmail.com>
2025-12-11 17:42:50 +0530

    feat: authenticate middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+import Session from "../models/sessionModel.js";
+import User from "../models/userModel.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/AsyncHandler.js";
+
+export const authenticate = asyncHandler(async (req, res, next) => {
+  const sessionId = req.signedCookies.sid;
+
+  if (!sessionId) {
+    throw new ApiError(401, "Unauthorized - No session found");
+  }
+
+  const session = await Session.findById(sessionId).lean();
+  if (!session) {
+    throw new ApiError(401, "Unauthorized - Invalid session");
+  }
+
+  const user = await User.findById(session.userId)
+    .select("-password -__v")
+    .lean();
+
+  if (!user) {
+    throw new ApiError(401, "Unauthorized - User not found");
+  }
+
+  req.user = user;
+  req.sessionId = session._id;
+
+  next();
+});
