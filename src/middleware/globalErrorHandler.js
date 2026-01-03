04bf042a2b9e1463886926d8f08217ca4bafc58b
anand maharana <anandmaharana427@gmail.com>
2026-01-03 12:59:56 +0530

    chore: condisnal retrun error response

--- src/middleware/globalErrorHandler.js
+++ src/middleware/globalErrorHandler.js
@@ -4,12 +4,12 @@ export const globalErrorHandler = (err, req, res, next) => {
   let message = err.message || "Internal Server Error";
 
   // Development logging (optional)
-  console.error("🔥 Error:", err);
+  if (statusCode >= 500) console.error("🔥 Error:", err);
 
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message,
+    message: statusCode >= 500 ? "Internal Server Error" : message,
     errors: err.errors || [],
     stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
