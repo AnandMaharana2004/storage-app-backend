f39ca3ec3e6b5e5e86bf2a4bccc01d0db2ed7964
anand maharana <anandmaharana427@gmail.com>
2026-01-08 15:31:14 +0530

    feat: add /trash router and comment out /share route

--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -5,30 +5,32 @@ import {
   GetFile,
   RenameFile,
   DeleteFile,
-  ShareFile,
-  GetPublicFile,
+  // ShareFile,
+  // GetPublicFile,
   MoveFile,
   MoveFileToTrash,
   GetFilesInDirectory,
   RemoveFromTrash,
   DownloadUrl,
+  GetAllTrashFiles,
 } from "../controller/fileController.js";
 import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // public route
-router.get("/shared/:fileId", GetPublicFile);
+// router.get("/shared/:fileId", GetPublicFile);
 
 // private routes
 router.use(authenticate);
 router.post("/upload/request", RequestUploadUrl);
 router.post("/upload/complete", CompleteUpload);
+router.get("/trash", GetAllTrashFiles);
 router.get("/:fileId", GetFile);
 router.get("/directory/:directoryId", GetFilesInDirectory);
 router.patch("/rename", RenameFile);
 router.patch("/move", MoveFile);
-router.post("/share", ShareFile);
+// router.post("/share", ShareFile);
 router.delete("/delete", DeleteFile);
 router.patch("/move-to-trash", MoveFileToTrash);
 router.patch("/remove-from-trash", RemoveFromTrash);
