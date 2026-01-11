4cf0f35cccd411a06702b8dee930c8910c8c9bfb
anand maharana <anandmaharana427@gmail.com>
2026-01-11 12:38:55 +0530

    feat: configuare routes for public share get and disable

--- src/routes/share.route.js
+++ src/routes/share.route.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
-import { ShareAssets } from "../controller/shareController.js";
+import {
+  CheckShare,
+  DisablePublicShare,
+  GetPublicFile,
+  SharePublicFile,
+} from "../controller/shareController.js";
 
 const router = Router();
 
-router.post("/", authenticate, ShareAssets);
+router.get("/public/exist", CheckShare);
+router.get("/public/:sharedToken", GetPublicFile);
+router.use(authenticate);
+// router.post("/", ShareAssets);
+router.post("/public", SharePublicFile);
+router.delete("/public/:fileId", DisablePublicShare);
 
 export default router;
